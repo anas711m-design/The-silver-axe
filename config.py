67642c5466191f0645c7be698eb644d27ba38887
anas711m-design <anas711m@gmail.com>
2026-04-05 00:35:15 +0300

    Change MOD_ROLE_ID in config.py
    Updated MOD_ROLE_ID to a new value.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # --- BOT CONFIGURATION ---
 # Replace the value below with your actual Mod Role ID
-MOD_ROLE_ID = 1490077861212852386
+MOD_ROLE_ID = 1490039067084066927
 
 # How long (in seconds) to remove the mod role as a penalty
 PENALTY_SECONDS = 120  # 2 minutes
